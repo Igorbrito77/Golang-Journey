9f93b0af0130bb44e0c68496a17171202fd067a7
Igorbrito77 <britoigor77@gmail.com>
2024-01-16 18:08:32 -0300

    Implementando função de transferência entre contas

--- banco/main.go
+++ banco/main.go
@@ -36,6 +36,23 @@ func (c* ContaCorrente) depositar(valorDeposito float64) (string, float64){
 
 }
 
+
+func (c* ContaCorrente ) transferir(valorTransferencia float64, contaDestino* ContaCorrente) (string, float64){
+
+	transferenciaPossivel := valorTransferencia > 0 && valorTransferencia <= c.saldo;
+
+	if(transferenciaPossivel){
+		contaDestino.saldo += valorTransferencia;
+		c.saldo -= valorTransferencia;
+		return "Transferência feita com sucesso", c.saldo;
+	} else{
+		return "Não foi possível efetuar a transição", c.saldo;
+	}
+
+}
+
+
+
 func main(){
 
 	var contaJorel *ContaCorrente;
@@ -45,14 +62,22 @@ func main(){
 	contaJorel.titular = "Jorel";
 	contaJorel.saldo = 1000;
 
+	var contaLurdes = ContaCorrente{titular: "Lurdes", saldo: 3500};
+
+
+
 	fmt.Println(*contaJorel);
 
 //	fmt.Println(contaJorel.sacar(-14));
 
-	fmt.Println(contaJorel.depositar(-400));
+	//fmt.Println(contaJorel.depositar(-400));
+
+	fmt.Println(contaLurdes.transferir(500, contaJorel));
 
 
 	fmt.Println(*contaJorel);
+	fmt.Println(contaLurdes);
+
 
 }
 
