6f72a5cbd707eff8e99ed0742dca59b26efcabfa
Igorbrito77 <britoigor77@gmail.com>
2024-01-17 11:24:53 -0300

    separando conta em novo package

---
+++ banco/contas/contas.go
@@ -0,0 +1,57 @@
+package contas;
+
+import(
+	"strconv"
+)
+
+
+type ContaCorrente struct{
+
+	Titular string
+	NumeroAgencia int
+	NumeroConta int
+	Saldo float64
+
+};
+
+
+func (c* ContaCorrente) sacar(valorSaque float64) string{
+
+	saquePossivel := valorSaque > 0 && c.Saldo >= valorSaque;
+
+	if(saquePossivel){
+		c.Saldo -= valorSaque;
+		return "Saque efetuado";
+	}else {
+		return "Não foi possível efetuar o Saque";
+	}
+}
+
+func (c* ContaCorrente) depositar(valorDeposito float64) (string, float64){
+
+	depositoPossivel := valorDeposito > 0;
+
+	if(depositoPossivel){
+		c.Saldo += valorDeposito;
+		return "Depósito feito com sucesso", c.Saldo;
+	} else{
+		return "Não foi possível efetuar o Saque", c.Saldo;
+	}
+
+}
+
+
+func (c* ContaCorrente ) Transferir(valorTransferencia float64, contaDestino* ContaCorrente) (string, float64){
+
+	transferenciaPossivel := valorTransferencia > 0 && valorTransferencia <= c.Saldo;
+
+	if(transferenciaPossivel){
+		contaDestino.Saldo += valorTransferencia;
+		c.Saldo -= valorTransferencia;
+		return "Transferência no valor de  "+ strconv.FormatFloat(valorTransferencia, 'f', -1, 64)+ " de " + c.Titular + " para o "+ contaDestino.Titular+  " feita com sucesso", c.Saldo;
+	} else{
+		return "Não foi possível efetuar a transição", c.Saldo;
+	}
+
+}
+
--- banco/main.go
+++ banco/main.go
@@ -1,78 +1,35 @@
 package main;
 
-import "fmt";
+import (
+	"fmt";
+	"banco/contas";
+);
 
-type ContaCorrente struct{
-
-	titular string
-	numeroAgencia int
-	numeroConta int
-	saldo float64
-
-};
-
-func (c* ContaCorrente) sacar(saque float64) string{
-
-	saquePossivel := saque > 0 && c.saldo >= saque;
-
-	if(saquePossivel){
-		c.saldo -= saque;
-		return "Saque efetuado";
-	}else {
-		return "Não foi possível efetuar o saque";
-	}
-}
-
-func (c* ContaCorrente) depositar(valorDeposito float64) (string, float64){
-
-	depositoPossivel := valorDeposito > 0;
-
-	if(depositoPossivel){
-		c.saldo += valorDeposito;
-		return "Depósito feito com sucesso", c.saldo;
-	} else{
-		return "Não foi possível efetuar o saque", c.saldo;
-	}
-
-}
-
-
-func (c* ContaCorrente ) transferir(valorTransferencia float64, contaDestino* ContaCorrente) (string, float64){
-
-	transferenciaPossivel := valorTransferencia > 0 && valorTransferencia <= c.saldo;
-
-	if(transferenciaPossivel){
-		contaDestino.saldo += valorTransferencia;
-		c.saldo -= valorTransferencia;
-		return "Transferência feita com sucesso", c.saldo;
-	} else{
-		return "Não foi possível efetuar a transição", c.saldo;
-	}
-
-}
 
 
 
 func main(){
 
-	var contaJorel *ContaCorrente;
+	var contaJorel *contas.ContaCorrente;
 
-	contaJorel = new(ContaCorrente);
+	contaJorel = new(contas.ContaCorrente);
 
-	contaJorel.titular = "Jorel";
-	contaJorel.saldo = 1000;
+	contaJorel.Titular = "Jorel";
+	contaJorel.Saldo = 1000;
 
-	var contaLurdes = ContaCorrente{titular: "Lurdes", saldo: 3500};
+	var contaLurdes = contas.ContaCorrente{Titular: "Lurdes", Saldo: 3500};
 
 
 
 	fmt.Println(*contaJorel);
+	fmt.Println(contaLurdes);
+
 
 //	fmt.Println(contaJorel.sacar(-14));
 
 	//fmt.Println(contaJorel.depositar(-400));
 
-	fmt.Println(contaLurdes.transferir(500, contaJorel));
+	fmt.Println(contaLurdes.Transferir(500.2412154545, contaJorel));
 
 
 	fmt.Println(*contaJorel);
