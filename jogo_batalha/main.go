96b21eb78dddb5a0912e42efc0ecea19574a4815
Igorbrito77 <britoigor77@gmail.com>
2024-01-27 14:06:06 -0300

    apagando funções não mais usadas

--- jogo_batalha/main.go
+++ jogo_batalha/main.go
@@ -2,7 +2,7 @@ package main;
 
 import (
 	"fmt";
-	"os";
+	//"os";
 	"jogo_batalha/personagem";
 	"jogo_batalha/jogador";
 	"jogo_batalha/inimigo";
@@ -15,25 +15,32 @@ import (
 
 // jogo de fases. com ataques aleatpórios. toda vez que passar de uma fase, criar uma fase nova trazendo um novo tipo de inimigo, e somando mais atk ao jogagor e ao inimigo seguinte;
 
-func main(){
 
+func criarLivro() livro.Livro{
 
 	inimigoFase1 := inimigo.Inimigo{};
 
 	inimigoFase1.Inicializar(personagem.Arqueiro);
 
-	fase1:= fase.Fase{ Inimigo : inimigoFase1, Numero: 1}
+	fase1:= fase.Fase{ Inimigo : inimigoFase1, Numero: 1};
 
-	//fase1:= Fase{ inimigos : [] inimigo.Inimigo {inimigoFase1}  }
 
-	fmt.Println(fase1);
+	inimigoFase2 := inimigo.Inimigo{};
 
-	livro := &livro.Livro{  Fases: [] fase.Fase{fase1} };
+	inimigoFase2.Inicializar(personagem.Barbaro);
+
+	fase2:= fase.Fase{ Inimigo : inimigoFase2, Numero: 2};
+
+
+	livro := livro.Livro{  Fases: [] fase.Fase{fase1, fase2} };
 	
-	
-	fmt.Println(livro);
+	return livro;
 
-	
+}
+
+func main(){
+
+	livro := criarLivro();
 
 	fmt.Println("Escolha uma classe de personagem: 1- Bárbaro, 2- Arqueiro");
 
@@ -55,136 +62,4 @@ func main(){
 
   	livro.IniciarHistoria(player);
  
-	//jogar(player);
-
-}
-
-func jogar(player * jogador.Jogador){
-
-	resultado := fase1(player);
-
-	if(resultado == -1){
-		os.Exit(0);
-	}
-
-	resultado = fase2(player);
-
-	
-
-}
-
-func fase1(player * jogador.Jogador) int{
-
-	var comando int;
-
-	inimigo := inimigo.Inimigo{};
-
-	inimigo.Inicializar(personagem.Arqueiro);
-
-	fmt.Println("\n\n Um inimigo ", inimigo.TipoPersonagem, " apareceu. Lute ou morra!");
-
-	inimigo.ExibirStatus();
-	player.ExibirStatus();
-
-	for inimigo.Vida >= 0 && player.Vida >= 0 {
-
-		// loop de batalha
-
-		fmt.Println("\n\n____________________________________________________________________________________________________________________________________________\n")
-		fmt.Println(" 								**** FASE 1! LUTE OU MORRA! **** \n")
-		fmt.Println("1- Atacar");
-		fmt.Println("2- Procurar item especial");
-		fmt.Println("0- Sair do jogo");
-		fmt.Println(" 								Escolha uma opção: \n")
-
-		fmt.Scan(&comando);
-
-		switch(comando){
-
-			case 1: 
-				player.Atacar(&inimigo);
-			case 2: 
-				player.AddItens("Bomba");
-				//player.AumentarAtk(10);
-			
-			default:
-				return -1;
-
-		}
-
-		inimigo.Atacar(player);
-		
-		inimigo.ExibirStatus();
-		player.ExibirStatus();
-
-	}
-
-	if(inimigo.Vida <= 0){
-		fmt.Println("Vitória! Você derrotou o inimigo!");
-
-		player.AumentarAtk(25);
-
-		return 1;
-	}else{
-		fmt.Println("Fim de Jogo! Você foi derrotado!");
-		return -1;
-	}
-}
-
-func fase2(player * jogador.Jogador) int{
-
-	var comando int;
-
-	inimigo := inimigo.Inimigo{};
-
-	inimigo.Inicializar(personagem.Barbaro);
-
-	fmt.Println("\n\n Um inimigo ", inimigo.TipoPersonagem, " apareceu. Lute ou morra!");
-
-	inimigo.ExibirStatus();
-	player.ExibirStatus();
-
-	for inimigo.Vida >= 0 && player.Vida >= 0 {
-
-		// loop de batalha
-
-		fmt.Println("\n\n____________________________________________________________________________________________________________________________________________\n")
-		fmt.Println(" 								**** FASE 2 ! LUTE OU MORRA! **** \n")
-		fmt.Println("1- Atacar");
-		fmt.Println("2- Procurar item especial");
-		fmt.Println("0- Sair do jogo");
-		fmt.Println(" 								Escolha uma opção: \n")
-
-		fmt.Scan(&comando);
-
-		switch(comando){
-
-			case 1: 
-				player.Atacar(&inimigo);
-			case 2: 
-				player.AddItens("Bomba");
-				//player.AumentarAtk(10);
-			
-			default:
-				return -1;
-
-		}
-
-		inimigo.Atacar(player);
-		
-		inimigo.ExibirStatus();
-		player.ExibirStatus();
-
-	}
-
-	if(inimigo.Vida <= 0){
-		fmt.Println("Vitória! Você derrotou o inimigo!");
-
-		player.AumentarAtk(25);
-
-		return 1;
-	}else{
-		fmt.Println("Fim de Jogo! Você foi derrotado!");
-		return -1;
-	}
 }
